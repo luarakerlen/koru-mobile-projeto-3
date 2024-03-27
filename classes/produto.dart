e46d1eb9872f50c20d8ed2e30e807d22fb6d5ef4
WillianSeidel <126173353+WillianSeidel@users.noreply.github.com>
2024-03-26 23:09:48 -0300

    feat: criar class produto (#3)

--- classes/produto.dart
+++ classes/produto.dart
@@ -1,3 +1,8 @@
 class Produto {
-  
+  final String nome;
+  double valor;
+  int qtdEmEstoque;
+  int qtdVendida;
+  Produto({required this.nome, required this.valor, required this.qtdEmEstoque})
+      : qtdVendida = 0;
 }
