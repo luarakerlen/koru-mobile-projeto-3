2cece2e06e2825185fd8f22ca16df0854bcccd0e
Evelyn Vanielly <67793057+evelyn-vanielly@users.noreply.github.com>
2024-03-27 19:25:58 -0300

    feat: criar metodo realizarVenda (#8)
    Co-authored-by: Vinicius Souza <vinicius.jsouza@yahoo.com.br>

--- classes/produto.dart
+++ classes/produto.dart
@@ -5,8 +5,20 @@ class Produto {
   int qtdVendida;
   Produto({required this.nome, required this.valor, required this.qtdEmEstoque})
       : qtdVendida = 0;
+
+  void realizarVenda() {
+    if (qtdEmEstoque >= qtdVendida) {
+      qtdVendida++;
+      qtdEmEstoque--;
+      print(
+          "Compra de um(a) produto $Produto realizada com sucesso!");
+    } else {
+      print(
+          'No momento não possuímos o produto $Produto em estoque.');
+    }
       
   double verReceitaGerada() {
     return valor * qtdVendida;
+develop
   }
 }
