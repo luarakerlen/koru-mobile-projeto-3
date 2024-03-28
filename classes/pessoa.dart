1d2cafcc004d5141d1957b27bef0914bf0368ccb
guifx <guifxb@yahoo.com>
2024-03-28 20:39:20 -0300

    feat: criação da classe Pessoa

---
+++ classes/pessoa.dart
@@ -0,0 +1,10 @@
+class Pessoa {
+  final String nome;
+  final String cpf;
+  final DateTime dataDeNascimento;
+
+  // Construtor
+  Pessoa(this.nome, this.cpf, this.dataDeNascimento); 
+
+  int? idade;
+}
