816a101a167d8e58ad3db920102739aef75384f8
Dayane99 <daystefane99@gmail.com>
2024-03-27 21:43:49 -0300

    feat: :sparkles: Criada classe produto
    Classe criada com todos os atributos requeridos pelo projeto

--- classes/produto.dart
+++ classes/produto.dart
@@ -1,3 +1,13 @@
 class Produto {
-  
+  final String nome;
+  double valor;
+  int qtdEmEstoque;
+  int qtdVendida = 0;
+
+  Produto({
+    required this.nome,
+    required this.valor,
+    required this.qtdEmEstoque,
+  });
 }
+
