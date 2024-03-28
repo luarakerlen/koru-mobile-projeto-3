61fdb74d12d2fcbc0feff3940f5875fd21c8a203
Fabio Reis <fabioreis.paz@gmail.com>
2024-03-28 15:06:07 -0300

    feat: :sparkles: funçao maiorIdade
    criação de função para informar se a pessoa é maior ou menor de idade

--- classes/pessoa.dart
+++ classes/pessoa.dart
@@ -9,4 +9,14 @@ class Pessoa {
     required this.dataDeNascimento,
     required this.cpf,
   });
+
+  void maiorIdade() {
+    final idade = calcularIdade();
+
+    if (idade >= 18) {
+      print('$nome tem $idade anos, portanto é maior de idade.');
+    } else {
+      print('$nome tem $idade anos, portanto é menor de idade.');
+    }
+  }
 }
