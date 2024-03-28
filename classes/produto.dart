f659550a2b3d9fb6b6b1ba978cc73262ed26499e
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2024-03-28 17:09:15 -0300

    feat: :sparkles: Criei o metodo raleizarVenda
    Metodo realizarVenda criado e já testado.
    
    cria um método novo.

--- classes/produto.dart
+++ classes/produto.dart
@@ -9,4 +9,18 @@ class Produto {
     required this.valor,
     required this.qtdEmEstoque,
   });
+
+  void realizarVenda() {
+    if (qtdEmEstoque < 0) {
+      throw ArgumentError('Quantidade inválida.');
+    } else if (qtdEmEstoque == 0) {
+      print('No momento não possuímos o produto $nome em estoque.');
+    } else {
+      qtdEmEstoque -= 1;
+      qtdVendida += 1;
+
+      print('Compra de um(a) produto $nome realizada com sucesso!');
+      print('Quantidade em estoque: $qtdEmEstoque');
+    }
+  }
 }
