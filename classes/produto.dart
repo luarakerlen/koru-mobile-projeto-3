056bb3e1f4d2ed1c164d04ee4159d0e91ac1e316
Dayane99 <daystefane99@gmail.com>
2024-03-28 10:53:30 -0300

    Revert "feat: :sparkles: Criada classe produto"
    This reverts commit 16439f59e0a05c147580ae6beb8ea7029c340b63.

--- classes/produto.dart
+++ classes/produto.dart
@@ -1,12 +1,3 @@
-class produto {
-  final String nome;
-  double valor;
-  int qtdEmEstoque;
-  int qtdVendida = 0;
-
-  produto({
-    required this.nome,
-    required this.valor,
-    required this.qtdEmEstoque,
-  });
+class Produto {
+  
 }
