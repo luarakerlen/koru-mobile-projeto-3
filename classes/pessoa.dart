62974e5187697d7663df15d30a33e44fb033bf41
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2024-03-29 15:59:25 -0300

    feat: chama o metodo no construtor e atribui seu retorno a idade

--- classes/pessoa.dart
+++ classes/pessoa.dart
@@ -1,4 +1,8 @@
 class Pessoa {
+  //construtor
+  Pessoa({required this.dataDeNascimento}) {
+    idade = calcularIdade();
+  }
   //inserir metodo calcularIdade (retorno: int)
   int calcularIdade() {
     return DateTime.now().difference(dataDeNascimento).inDays ~/ 365.toInt();
