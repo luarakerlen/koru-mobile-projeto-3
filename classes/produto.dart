78c6f97d1084c4b58314354510ea4817b0284f99
jcr04 <joaocarlosreboucas15@gmail.com>
2024-03-31 11:06:51 -0300

    feat: - add método realizarVenda

--- classes/produto.dart
+++ classes/produto.dart
@@ -1,4 +1,14 @@
 class Produto {
   int qtdEmEstoque = 0;
   late int qtdVendida;
+
+  void realizarVenda() {
+    if (qtdEmEstoque > 0) {
+      qtdVendida++;
+      qtdEmEstoque--;
+      print("Compra de um(a) produto $nome realizada com sucesso!");
+    } else {
+      print("No momento não possuímos o produto $nome em estoque.");
+    }
+  }
 }
