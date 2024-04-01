9e57e49a860c23139cfe3fe2023eb14ab8df2967
Igor Fortti <igor.fortti@positivo.com.br>
2024-03-31 23:11:39 -0300

    Adicionado os atributos dataDeNascimento e idade e os métodos calcularIdade e maioridade.

--- classes/pessoa.dart
+++ classes/pessoa.dart
@@ -1,9 +1,33 @@
 class Pessoa {
   String nome;
   String cpf;
-  Pessoa(this.nome, this.cpf);
+  DateTime dataDeNascimento;
+  late int idade;
+
+  Pessoa(this.nome, this.cpf, this.dataDeNascimento) {
+    idade = calcularIdade();
+  }
 
   void falar(String fala) {
     print(fala);
+  }
+
+  int calcularIdade() {
+    DateTime agora = DateTime.now();
+    int idade = agora.year - dataDeNascimento.year;
+    if (agora.month < dataDeNascimento.month ||
+        (agora.month == dataDeNascimento.month &&
+            agora.day < dataDeNascimento.day)) {
+      idade--;
+    }
+    return idade;
+  }
+
+  void maioridade() {
+    if (idade >= 18) {
+      print('$nome tem $idade anos, portanto é maior de idade.');
+    } else {
+      print('$nome tem $idade anos, portanto é menor de idade.');
+    }
   }
 }
