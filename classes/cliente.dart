8950746e89a7dba9e1662ffdab401dfe7c8179fe
Andréa <123258962+devdados@users.noreply.github.com>
2024-04-01 22:31:06 -0300

    feat: criar classe cliente (#18)

---
+++ classes/cliente.dart
@@ -0,0 +1,14 @@
+import 'pessoa.dart';
+import 'produto.dart';
+
+class Cliente extends Pessoa {
+  double dinheiro;
+  List<Produto> produtosComprados = [];
+
+  Cliente(
+      {required String nome,
+      required String cpf,
+      required DateTime dataDeNascimento,
+      this.dinheiro = 0})
+      : super(nome: nome, cpf: cpf, dataDeNascimento: dataDeNascimento);
+}
