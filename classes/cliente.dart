9ff25a7e786cb1c07e5dbd3074bcae58eff3cdc4
Petrônio Oliveira <136655989+petronio50@users.noreply.github.com>
2024-04-02 22:02:16 -0300

    feat: criar método adicionar dinheiro (#21)

--- classes/cliente.dart
+++ classes/cliente.dart
@@ -16,4 +16,13 @@ class Cliente extends Pessoa {
   void falar(String falaDaPessoa) {
     print('Cliente $nome diz: $falaDaPessoa');
   }
+
+  void adicionarDinheiro(double valor) {
+    if (valor >= 0) {
+      dinheiro += valor;
+      print('$nome agora você possui ${dinheiro.toStringAsFixed(2)} reais.');
+    } else {
+      print('$nome não é permitido fazer esse tipo de Operação.');
+    }
+  }
 }
