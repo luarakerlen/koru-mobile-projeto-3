f406d91bee53c59f48739f2632de2b8db71d7b20
matheusqueirozdev <dev.matheusqueiroz@gmail.com>
2024-04-03 15:43:13 -0300

    feat: Criado funções vazias para evitar os conflitos e também fazendo alguns imports e colocando genero nos construtores das classes que herdaram pessoa
    Criado funções vazias para evitar os conflitos e também fazendo alguns imports e colocando genero nos construtores das classes que herdaram pessoa

--- classes/cliente.dart
+++ classes/cliente.dart
@@ -1,3 +1,4 @@
+import 'enums.dart';
 import 'pessoa.dart';
 import 'produto.dart';
 
@@ -9,10 +10,19 @@ class Cliente extends Pessoa {
     required String nome,
     required DateTime dataDeNascimento,
     required String cpf,
+    required Genero genero,
     this.dinheiro,
   }) : super(
           nome: nome,
           dataDeNascimento: dataDeNascimento,
           cpf: cpf,
+          genero: genero,
         );
+
+  @override
+  void falar(String mensagem) {}
+
+  void adicionarDinheiro() {}
+
+  void comprarProduto() {}
 }
