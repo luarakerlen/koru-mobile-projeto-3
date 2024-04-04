eea7fb5af63044547b29da2943f7862d6604297c
AlexandreCalmonJr <87951095+AlexandreCalmonJr@users.noreply.github.com>
2024-04-03 21:34:16 -0300

    feat: criar metodo falar classe revendedor (#25)

--- classes/revendedor.dart
+++ classes/revendedor.dart
@@ -13,11 +13,31 @@ class Revendedor extends Pessoa {
       required DateTime dataDeNascimento,
       required this.matricula,
       required Genero genero})
-      : super(nome: nome, cpf: cpf, dataDeNascimento: dataDeNascimento, genero:genero);
+      : super(
+            nome: nome,
+            cpf: cpf,
+            dataDeNascimento: dataDeNascimento,
+            genero: genero);
 
-      
-      void venderProduto(Produto produto){
-        produto.realizarVenda();
-        produtosVendidos.add(produto);
-      }
+  void venderProduto(Produto produto) {
+    produto.realizarVenda();
+    produtosVendidos.add(produto);
+  }
+
+  @override
+  void falar(String falaDaPessoa) {
+    String generoRevendedor;
+    switch (genero) {
+      case Genero.Feminino:
+        generoRevendedor = 'Revendedora';
+        break;
+      case Genero.Masculino:
+        generoRevendedor = 'Revendedor';
+        break;
+      case Genero.Outro:
+        generoRevendedor = 'Pessoa revendedora';
+        break;
+    }
+    print('$generoRevendedor $nome diz: $falaDaPessoa');
+  }
 }
