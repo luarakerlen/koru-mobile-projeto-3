0dc3b3decce2bd651d709e3d39a3dd022292d94b
Daniela <danielasouza0729@gmail.com>
2024-04-05 17:56:54 -0300

    feat: Implementado atributo array produtos comprados

--- classes/cliente.dart
+++ classes/cliente.dart
@@ -2,7 +2,7 @@ import 'pessoa.dart';
 
 class Cliente extends Pessoa{
   double dinheiro;
-
+  List<String>produtoComprados = [];
 
   Cliente (super.nome, super.cpf, super.dataDeNascimento, super.generos,[this.dinheiro=0.0]);
 
