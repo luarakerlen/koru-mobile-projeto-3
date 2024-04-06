ce2080de662a92bcefe851a8077a438a674f5074
VINÖCIUS\marcu <marcus_matoso@hotmail.com>
2024-04-06 15:20:38 -0300

    feat: criação do metodo comprarProduto

--- classes/cliente.dart
+++ classes/cliente.dart
@@ -1,8 +1,10 @@
 import 'pessoa.dart';
+import 'produto.dart';
+import 'revendedor.dart';
 
 class Cliente extends Pessoa{
   double dinheiro;
-    List<String>produtoComprados = [];
+  List<String>produtoComprados = [];
 
   Cliente (super.nome, super.cpf, super.dataDeNascimento, super.generos,[this.dinheiro=0.0]);
 
@@ -10,9 +12,20 @@ class Cliente extends Pessoa{
   void falar(String fala) {
     print("Cliente ${super.nome} diz: $fala");
   }
+
   void adicionarDinheiro( double valor){
 
     this.dinheiro += valor;
     print('Agora a pessoa ${super.nome} possui: ${this.dinheiro.toStringAsFixed(2)} reais.');
+  }
+
+  void comprarProduto(Produto produto, Revendedor revendedor){
+      if (dinheiro >= produto.valor) {
+        dinheiro -= produto.valor;
+        produtoComprados.add(produto.nome);
+        print('${super.nome} comprou o produto ${produto.nome} por ${produto.valor.toStringAsFixed(2)} reais.');
+      } else {
+        print('${super.nome} não possui dinheiro suficiente para comprar o produto ${produto.nome}.');
+      }
   }
 }
