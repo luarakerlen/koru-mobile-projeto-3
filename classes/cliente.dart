8a32b2629743626d7d3437783c899c1867244ec4
jaquedreyer <jaqueline.bbdreyer@gmail.com>
2024-04-06 10:42:26 -0300

    feat: adiciona metodo comprar produto na classe cliente

--- classes/cliente.dart
+++ classes/cliente.dart
@@ -1,3 +1,15 @@
 import 'pessoa.dart';
 
-class Cliente extends Pessoa {}
+class Cliente extends Pessoa {
+  //metodo comprarProduto (retorno: void)
+  void comprarProduto(Produto produto, Revendedor revendedor) {
+    if (dinheiro >= produto.valor) {
+      revendedor.venderProduto(produto);
+      dinheiro -= produto.valor;
+      produtosComprados.add(produto);
+    } else {
+      print(
+          '$nome não possui dinheiro suficiente para comprar o produto ${produto.nome}');
+    }
+  }
+}
