ca1b9428185b772a9ee4db90eec823d3ee9eba19
Flavslg <58560269+Flavslg@users.noreply.github.com>
2024-04-07 19:22:04 -0300

    feat: Criação do método Adicionar Dinheiro

--- classes/cliente.dart
+++ classes/cliente.dart
@@ -13,4 +13,14 @@ Cliente(super.nome,
         super.genero,
         {this.dinheiro = 0});
 
+
+//Criação do método Adicionar Dinheiro
+        void adicionarDinheiro (double valor){
+	        if (valor > 0){
+	                dinheiro = dinheiro + valor;
+	                print("$nome agora possui R\$ ${dinheiro.toStringAsFixed(2)}.");
+	                }
+                }
+
+        
 }
