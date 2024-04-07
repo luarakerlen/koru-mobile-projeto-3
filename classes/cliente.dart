009580f8cdd3ffb9ecbd2f4bb70205cae3133adf
Flavslg <58560269+Flavslg@users.noreply.github.com>
2024-04-07 18:15:41 -0300

    fix: atributo dinheiro
    - Ajuste da variável dinheiro para não nullable;
    - Correção do parametro opcional dinheiro dentro do construtor;

--- classes/cliente.dart
+++ classes/cliente.dart
@@ -1,11 +1,16 @@
 import 'pessoa.dart';
-import 'produto.dart';
 import '../enums.dart';
+import 'produto.dart';
+import 'revendedor.dart';
 
 class Cliente extends Pessoa {
-double? dinheiro;
+double dinheiro;
 List<Produto> produtosComprados = [];
 
-Cliente.optional(super.nome, super.cpf, super.dataDeNascimento, super.genero, {this.dinheiro = ""});
+Cliente(super.nome,
+        super.cpf,
+        super.dataDeNascimento,
+        super.genero,
+        {this.dinheiro = 0});
 
 }
