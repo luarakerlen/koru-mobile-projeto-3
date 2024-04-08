163a3c3edd810f51b435c5e368b8a956487b0395
kamilateofilo <123755464+kamilateofilo@users.noreply.github.com>
2024-04-08 15:30:59 -0300

    Feat: Extends Class Pessoa
    Correção

--- classes/cliente.dart
+++ classes/cliente.dart
@@ -1,14 +1,16 @@
-class Cliente {
+import 'pessoa.dart';
+
+class Cliente extends Pessoa{
   double dinheiro;
 
-  Cliente([this.dinheiro = 0.0]);
+  Cliente({required super.nome, required super.cpf, required super.dataDeNascimento, this.dinheiro = 0.0});
 
   void adicionarDinheiro(double? valor) {
     if (valor != null) {
       dinheiro += valor;
-      print("Maria adicionou $valor à sua carteira. Agora você tem $dinheiro em dinheiro.");
+      print("$nome adicionou $valor à sua carteira. Agora você tem $dinheiro em dinheiro.");
     } else {
-      print("O valor adicionado foi 'null'.");
+      print("Nenhum valor adicionado à carteira");
     }
   }
 }
