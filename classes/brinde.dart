940ea9d91f35be384126f2b0d623ec7f737ecbec
jcr04 <joaocarlosreboucas15@gmail.com>
2024-04-17 19:51:51 -0300

    feat: - adcionando método ordernaBrindes

--- classes/brinde.dart
+++ classes/brinde.dart
@@ -1,7 +1,13 @@
-
 class Brinde {
-
   void consultarPontosNecessarios() {
     print('Para ganhar um(a) $nome você precisa de $pontosNecessarios pontos');
+  }
+
+  static void ordenarBrindes(List<Brinde> brindes) {
+    brindes.sort((a, b) => a.nome.compareTo(b.nome));
+    print("Brindes ordenados:");
+    for (var brinde in brindes) {
+      print("${brinde.nome} - ${brinde.pontosNecessarios} pontos");
+    }
   }
 }
