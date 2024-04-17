abf23d72abdbcfab5fe5c5bc602ceedf7218a58c
Franciéllen Sousa Araújo <franciellensousaaraujo@gmail.com>
2024-04-17 14:31:44 -0300

    fix: Corrigir chamada da função

--- classes/cliente.dart
+++ classes/cliente.dart
@@ -64,14 +64,14 @@ class Cliente extends Pessoa {
     }
   }
 
-/*
+
   void verResumo(double valorTotalGasto, double valorMedioGasto) {
     print(
         "O total gasto por ${nome} foi ${fixarDuasCasasDecimais(calcularTotalGasto())} reais e a média de valor dos produtos comprados é ${fixarDuasCasasDecimais(calcularMediaProdutosComprados())} reais.");
-  }*/
+  }
   
   void verBrindes() {
-    ordenarBrinde();
+    ordenarBrindes();
     print('Brindes recebidos por $nome:');
     brindes.forEach((brinde) {
       print('$brinde');
